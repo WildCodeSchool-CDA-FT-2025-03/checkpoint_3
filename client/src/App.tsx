1f639682fb75beab9ac72941c93a5ec09aa6ec98
Maximilien <maximilien.philippe@protonmail.com>
2025-05-26 12:29:07 +0200

    Add AddCountry component to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import "./index.css";
 import Header from "./components/Header";
 import CountriesList from "./components/CountriesList";
+import AddCountry from "./components/AddCountries/AddCountry";
 
 function App() {
   return (
     <>
       <div className="p-[2px]">
         <Header />
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center">
+          <AddCountry />
           <CountriesList />
         </div>
       </div>
