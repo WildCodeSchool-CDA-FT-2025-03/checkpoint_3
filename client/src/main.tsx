59c9f92caf24fdc84a0ad39696c030ec83ea8cbb
Dilgo-dev <dilgopierre@gmail.com>
2025-05-26 09:55:36 +0200

    feat: Integrate ApolloProvider in main.tsx for GraphQL context support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "antd/dist/reset.css";
+import { ApolloProvider } from "@apollo/client";
+
 import App from "./App.tsx";
+import client from "./services/client.ts";
+
+import "antd/dist/reset.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
