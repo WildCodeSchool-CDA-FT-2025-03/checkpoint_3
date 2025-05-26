b1616a8c734357050ffdfbd6a6ac5b6df57146c8
Maximilien <maximilien.philippe@protonmail.com>
2025-05-26 12:14:06 +0200

    Add CountriesList to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import "./index.css";
 import Header from "./components/Header";
+import CountriesList from "./components/CountriesList";
 
 function App() {
   return (
     <>
       <div className="p-[2px]">
         <Header />
+        <div className="flex justify-center">
+          <CountriesList />
+        </div>
       </div>
     </>
   );
