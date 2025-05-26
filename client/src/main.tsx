dad77623799758d539d510566d9d4bc5a67b6e47
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-26 10:24:28 +0200

    Configuration de mon environnement

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const dataRouter = [
+  /* {ListDataRouter[0].path} */
+    {
+        path: "/",
+        element: <h1>page</h1>,
+    },
+]
+
+const router = createBrowserRouter([
+    {
+     element: <App />,
+     children: dataRouter
+    },
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+createRoot(document.getElementById('root')!).render(
+     <StrictMode>
+       <RouterProvider router={router} />
+     </StrictMode>
+)
