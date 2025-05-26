399daccbcce8c4432f75c6848cbdab4c1559c88f
Yiroma <yiromaric@gmail.com>
2025-05-26 10:51:27 +0200

    env@3-Configuration graphQL - Codegen

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo-client.ts";
 
 import App from "./App.tsx";
 
@@ -7,6 +9,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
