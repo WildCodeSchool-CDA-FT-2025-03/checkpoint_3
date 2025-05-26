3de162b1545a410af9a80626ec956ba4101874be
Florian Sebal <fsebal91@gmail.com>
2025-05-26 11:05:14 +0200

    Pages listpays et Detailpays

---
+++ client/src/pages/detailpays.tsx
@@ -0,0 +1,8 @@
+const Detailpays = () => {
+  return (
+    <div>
+      <h1>Detail Pays</h1>
+    </div>
+  );
+};
+export default Detailpays;
---
+++ client/src/pages/listpays.tsx
@@ -0,0 +1,8 @@
+const Listpays = () => {
+  return (
+    <div>
+      <h1>Listing Pays</h1>
+    </div>
+  );
+};
+export default Listpays;
