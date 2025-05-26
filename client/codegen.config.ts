f146f04080c26e53ab667a9a9c913c373625699b
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-05-26 10:21:21 +0200

    Configuration graphQL Client - Codegen

---
+++ client/codegen.config.ts
@@ -0,0 +1,19 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'http://localhost:4000',
+  documents: ['src/schemas/**/*.{ts,tsx}'],
+  generates: {
+    './src/types/graphql-generated.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      config: {
+        withHooks: true,
+        withComponent: false,
+        withHOC: false,
+      },
+    },
+  },
+  overwrite: true,
+};
+
+export default config;
