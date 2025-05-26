bb073f3b2c041689ca6f817aca9d5fe4cd30ba8a
Dilgo-dev <dilgopierre@gmail.com>
2025-05-26 10:20:31 +0200

    feat: Replace old GraphQL code generation configuration with a new setup for improved type generation and document handling

---
+++ client/codegen.ts
@@ -0,0 +1,22 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://backend:4000",
+  documents: ["src/schemas/**/*.{ts,tsx}"],
+  generates: {
+    "./src/types/graphql-generated.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true,
+        withComponent: false,
+        withHOC: false,
+      },
+    },
+  },
+};
+
+export default config;
